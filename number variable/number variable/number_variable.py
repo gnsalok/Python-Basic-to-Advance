c66cfeb2a6046a15144a82f26ba9befbc2d50396
Alok Tripathi <gnsalok@outlook.com>
2017-09-23 10:42:45 +0530

    Working with numbers

--- number variable/number variable/number_variable.py
+++ number variable/number variable/number_variable.py
@@ -1,25 +1,33 @@
 
-print("Calculate the area of triangle\n")
+#print("Calculate the area of triangle\n")
 
 
-area=0
-height=10.12020
-width=20
+#area=0
+#height=10.12020
+#width=20
 
-area=height*width/2
+#area=height*width/2
 
 
-print("Area of Triangle is : %d"%area+"\n")
-#gives 5 white spaces
-print("Area of Triangle is : %5d"%area+"\n")
-#float type values 
-print("Area of Triangle is : %f"%area+"\n")
-#two digit after no.
-print("Area of Triangle is : %.2f"%area+"\n")
+#print("Area of Triangle is : %d"%area+"\n")
+##gives 5 white spaces
+#print("Area of Triangle is : %5d"%area+"\n")
+##float type values 
+#print("Area of Triangle is : %f"%area+"\n")
+##two digit after no.
+#print("Area of Triangle is : %.2f"%area+"\n")
 
 #use of .format method
-print("I have {0:d} cats ".format(9,10,14)+"\n")
-print("I have {1:d} cats ".format(9,10,14)+"\n")
-print("I have {2:d} cats ".format(9,10,14)+"\n")
-print("I have {0:f} cats ".format(9,10,14)+"\n")
+#print("I have {0:d} cats ".format(9,10,14)+"\n")
+#print("I have {1:d} cats ".format(9,10,14)+"\n")
+#print("I have {2:d} cats ".format(9,10,14)+"\n")
+#print("I have {0:f} cats ".format(9,10,14)+"\n")
 
+
+
+
+#Adding two numbers..
+num1=int(input('Enter number 1 : '))
+num2=int(input("Enter number 2 : "))
+sum=num1+num2
+print("Sum of  {0:d} and {1:d} is : {2:d}".format(num1,num2,sum))
