9292d29294110de1df1705c85a482f05cdc8cc07
Alok Tripathi <gnsalok@gmail.com>
2017-10-03 20:18:20 +0530

    woking with conditional staements i.e. if, ifelif etc

---
+++ 4.Conditional Statements/if_elif.py
@@ -0,0 +1,13 @@
+#working with elif
+
+message = "Hey"
+
+
+if message == "hi":
+    print("says hi")
+    
+elif message == "Hey":
+    print("Says Hey")    
+    
+else:
+    print("Nothing says")
---
+++ 4.Conditional Statements/ternary_operator.py
@@ -0,0 +1,9 @@
+#Working with ternary operator..
+
+#It used to assiging the value to a variable
+
+value=int(input('Enter you value?'))
+
+EvenOrOdd = "Even" if value%2==0 else "Odd"
+
+print(EvenOrOdd)
