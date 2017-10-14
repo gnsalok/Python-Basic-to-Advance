bfeab3aeaf76d4dad8970b89d474f5dc8f5b16e8
Alok Tripathi <gnsalok@gmail.com>
2017-10-14 20:36:06 +0530

    Inheritance , Method overloading vs overriding

--- 10.OOPS in Python/Class and Object.py
+++ 10.OOPS in Python/Class and Object.py
@@ -11,18 +11,18 @@ print(C.real, C.imag)
 
 
 '''
-#Test Yourself
-class Employee:  
-    Name=""
-    Id=""
-    def __init__(self,name,id):
-        self.Name=name  #self in used as this in another language like C#,Java and javascript
-        self.Id=id
-        
-    def showEmployee(self): #This is gonna to class Method or action
-        print("Name is {0} and ID is: {1}  ".format(self.Name,self.Id))
-        
-E=Employee("Alok Tripathi" , 1)
-E.showEmployee()
+##Test Yourself
+#class Employee:  
+#    Name=""
+#    Id=""
+#    def __init__(self,name,id):
+#        self.Name=name  #self in used as this in another language like C#,Java and javascript
+#        self.Id=id
+#        
+#    def showEmployee(self): #This is gonna to class Method or action
+#        print("Name is {0} and ID is: {1}  ".format(self.Name,self.Id))
+#        
+#E=Employee("Alok Tripathi" , 1)
+#E.showEmployee()
 
 '''
