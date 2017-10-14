cecb53b23f7c7a62e264964e2a704f9447ff66df
Alok Tripathi <gnsalok@gmail.com>
2017-10-14 19:36:05 +0530

    working with exception handling...

---
+++ 7.Exception Handling/exception-handling.py
@@ -0,0 +1,27 @@
+#Working with Exception : Exception Handling ...
+
+'''
+
+try:    
+    print(5/0)
+    
+except Exception as e: 
+    print(e) 
+
+finally:
+    print("Finally block executed!!")
+
+'''
+
+
+try:  
+    value=int(input("Enter an integer value?"))
+
+except Exception as e:
+    print(e)
+    
+finally:
+    print("typecasting successfull")
+
+
+
