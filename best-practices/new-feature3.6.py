f0f9cf0a6fae149578e56d5eaf9dee54a7c83eb0
Alok Tripathi <gnsalok@gmail.com>
2018-11-16 17:41:06 +0530

    new features of python 3.6 and more in new-feature3.6.y file

---
+++ best-practices/new-feature3.6.py
@@ -0,0 +1,20 @@
+'''
+name=input()
+# 3.6 feature in python 
+print(f"Name is : {name}!")
+'''
+
+'''
+tuple =(12.0,14.6)
+list=['one','two','three']
+print(f'{tuple}  {list} ')
+'''
+
+# for i in range(5):
+#     print(i)
+
+
+set = {1,2,2,4,5}
+set.add(12)
+print(set)
+
