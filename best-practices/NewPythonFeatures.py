9d251d8c8b19fa3c9719df14e2eb0002ac7d7f94
Alok Tripathi <gnsalok@gmail.com>
2018-11-16 18:18:48 +0530

    importing functions from different modules

---
+++ best-practices/NewPythonFeatures.py
@@ -0,0 +1,36 @@
+'''
+name=input()
+# 3.6 feature in python 
+print(f"Name is : {name}!")
+'''
+
+'''
+tuple =(12.0,14.6)
+list=['one','two','three']
+print(f'{tuple}  {list} ')
+'''
+
+# for i in range(5):
+#     print(i)
+'''
+
+set = {1,2,2,4,5}   # can't add duplicate value
+set.add(12)  #append the set
+print(set)
+
+
+'''
+
+# dict = {'alice':12,'Mile':15}
+# print(dict)
+
+'''
+
+def square(x):
+    return x*x
+
+for i in range(5):
+    print('{} square is {}'.format(i,square(i)))
+
+'''
+
---
+++ best-practices/test.py
@@ -0,0 +1,3 @@
+from modules import square
+
+print(square(10))
