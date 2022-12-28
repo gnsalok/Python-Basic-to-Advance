55ead0f5c15ef482fd7123a84a6ee67124e3ebb4
Alok Tripathi <gnsalok@gmail.com>
2022-12-28 18:38:09 +0530

    testing

--- PyConcepts/map&filter.py
+++ PyConcepts/map&filter.py
@@ -1,6 +1,3 @@
-# Map and filter
-# custom sorting
-
 items = [
     ("Product1", 10),
     ("Product2", 9),
@@ -9,18 +6,13 @@ items = [
 
 # simple sorting
 items.sort(key=lambda item: item[1])
+print(items)
 
 #Map and filter
-prices = list(map(lambda items: item[1], items))
+prices = list(map(lambda item: items[1], items))
+print(prices)
+
+
+
 expensive_items = list(filter(lambda item: item[1] >= 10, items))
-
-
-# #Example
-
-# values = [2,3,4,5,6]
-
-# def double(x):
-#     return x*2
-
-# result = list(map(double,values))
-# print(result)
+print(expensive_items)
--- test.py
+++ test.py
@@ -1,12 +1,18 @@
-class Employee:
-    def __init__(self):
-        self.name='Swati'
-        self.salary=10000
+items = [
+    ("Product1", 10),
+    ("Product2", 9),
+    ("Product3", 11)
+]
 
-    def __str__(self):
-        return 'name='+self.name+' salary=$'+str(self.salary)
+# simple sorting
+items.sort(key=lambda item: item[1])
+print(items)
+
+#Map and filter
+prices = list(map(lambda item: items[1], items))
+print(prices)
 
 
 
-emp = Employee()
-print(emp)
+expensive_items = list(filter(lambda item: item[1] >= 10, items))
+print(expensive_items)
